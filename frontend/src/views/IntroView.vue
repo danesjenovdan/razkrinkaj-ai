b83596a856bd219fdb0ebfa37fbbfce9a0f0bc66
Patricija Brečko <patricija@danesjenovdan.si>
2024-10-25 08:05:48 +0200

    intro view

--- frontend/src/views/IntroView.vue
+++ frontend/src/views/IntroView.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useIntroductionStore } from '@/stores/introduction'
+import { useStore } from '@/stores/store'
 
-const store = useIntroductionStore()
+const store = useStore()
+store.initializeStore()
 
 </script>
 
 <template>
   <main>
     <div>
-      <h1>{{ store.title }}</h1>
-      {{ store.introduction }}
+      <h1>{{ store.introduction_title }}</h1>
+      <div v-html="store.introduction_description"></div>
     </div>
     <div class="button-wrapper">
-      <RouterLink :to="{ name: 'chapters-list' }" class="button">ZAČNI</RouterLink>
+      <RouterLink :to="{ name: 'chapters-list' }" class="button">{{ store.introduction_button_text }}</RouterLink>
     </div>
   </main>
 </template>
