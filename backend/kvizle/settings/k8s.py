65027323a9b26055772dd7c2119f8df2c62b2d45
LepkoQQ <miha@lepko.net>
2026-02-20 20:36:03 +0100

    cleanup, remove some old stuff and update django files to version 6.0

--- backend/kvizle/settings/k8s.py
+++ backend/kvizle/settings/k8s.py
@@ -1,8 +1,8 @@
 from .base import *
 
-DEBUG = bool(os.getenv("DJANGO_DEBUG", False))
+DEBUG = bool(os.getenv("DJANGO_DEBUG", ""))
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "<TODO>")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "")
 
 ALLOWED_HOSTS = ["api.kvizle.lb.djnd.si"]
 CSRF_TRUSTED_ORIGINS = ["https://api.kvizle.lb.djnd.si"]
@@ -10,22 +10,28 @@ CORS_ALLOWED_ORIGINS = ["https://kvizle.lb.djnd.si"]
 
 WAGTAILADMIN_BASE_URL = "https://api.kvizle.lb.djnd.si"
 
-STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", os.path.join(BASE_DIR, "static"))
+STATIC_ROOT = os.getenv("DJANGO_STATIC_ROOT", BASE_DIR / "static")
 STATIC_URL = os.getenv("DJANGO_STATIC_URL_BASE", "/static/")
 
+# Storage backends temp variables
+_msfs = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+_s3s = "storages.backends.s3.S3Storage"
+
+# ManifestStaticFilesStorage is recommended in production, to prevent
+# outdated JavaScript / CSS assets being served from cache
+# (e.g. after a Wagtail upgrade).
+# See https://docs.djangoproject.com/en/6.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
+STORAGES["staticfiles"]["BACKEND"] = _msfs
+
 if os.getenv("DJANGO_ENABLE_S3", False):
-    STORAGES["default"] = {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-    }
-    AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID", "<TODO>")
-    AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY", "<TODO>")
+    STORAGES["default"]["BACKEND"] = _s3s
+    AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME", "djnd")
     AWS_DEFAULT_ACL = "public-read"
     AWS_QUERYSTRING_AUTH = False
     AWS_LOCATION = os.getenv("DJANGO_AWS_LOCATION", "kvizle")
-    AWS_S3_REGION_NAME = os.getenv("DJANGO_AWS_REGION_NAME", "fr-par")
-    AWS_S3_ENDPOINT_URL = os.getenv(
-        "DJANGO_AWS_S3_ENDPOINT_URL", "https://s3.fr-par.scw.cloud"
-    )
-    AWS_S3_SIGNATURE_VERSION = os.getenv("DJANGO_AWS_S3_SIGNATURE_VERSION", "s3v4")
+    AWS_S3_REGION_NAME = "fr-par"
+    AWS_S3_ENDPOINT_URL = "https://s3.fr-par.scw.cloud"
+    AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_S3_FILE_OVERWRITE = False
