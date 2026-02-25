d2a85c6fb029344f028a82c26fc9b1657578a314
LepkoQQ <miha@lepko.net>
2026-02-25 14:05:37 +0100

    fix scss deprecations from dep

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -25,4 +25,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["if-function"], // for sass-fairy
+      },
+    },
+  },
 });
